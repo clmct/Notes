0aed9df26f4b0a9fb0f7bb43fb197bfda90b12c2
Almat <almat.kulbaev@yandex.ru>
2021-03-13 16:11:20 +0700

    init

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Алмат Кульбаев on 13.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
